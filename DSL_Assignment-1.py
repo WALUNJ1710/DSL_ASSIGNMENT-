a17e553a5f1f9bb3c7cf6a2e2dd184f0e00f513e
WALUNJ1710 <92929453+WALUNJ1710@users.noreply.github.com>
2021-11-10 21:11:11 +0530

    Update DSL_Assignment-1.py

--- DSL_Assignment-1.py
+++ DSL_Assignment-1.py
@@ -1,116 +1,115 @@
 def removeDuplicate(d):
-    lst=[]
+    L=[]
     for i in d:
-        if i not in lst:
-            lst.append(i)
-    return lst
+        if i not in L:
+            L.append(i)
+    return L
 
 # Function for finding intersection between two sets (A&B)
 
-def intersection(lst1,lst2):
-    lst3=[]
-    for val in lst1:
-        if val in lst2:
-            lst3.append(val)
-    return lst3
+def intersection(L1,L2):
+    L3=[]
+    for val in L1:
+        if val in L2:
+            L3.append(val)
+    return L3
 
 # Function for finding union of two sets (A|B)
 
-def union(lst1,lst2):
-    lst3=lst1.copy()
-    for val in lst2:
-        if val not in lst3:
-            lst3.append(val)
-    return lst3
+def union(L1,L2):
+    L3=L1.copy()
+    for val in L2:
+        if val not in L3:
+            L3.append(val)
+    return L3
 
 # Function for finding difference between two sets (A-B)
-
-def diff(lst1,lst2):
-    lst3=[]
-    for val in lst1:
-        if val not in lst2:
-            lst3.append(val)
-    return lst3
+def diff(L1,L2):
+    L3=[]
+    for val in L1:
+        if val not in L2:
+            L3.append(val)
+    return L3
 
 # Function for finding symmetric difference of two sets (A^B)
 
-def sym_diff(lst1,lst2):
-    lst3=[]
-    D1=diff(lst1,lst2)
+def sym_diff(L1,L2):
+    L3=[]
+    D1=diff(L1,L2)
     print("Difference between Cricket and Badminton (C-B) is : ", D1)
-    D2=diff(lst2,lst1)
+    D2=diff(L2,L1)
     print("Difference between Badminton and Cricket (B-C) is : ", D2)
-    lst3=union(D1,D2)
-    return lst3
+    L3=union(D1,D2)
+    return L3
 
-# Functon for finding List of students who play both cricket and badminton
+# Function for finding List of students who play both cricket and badminton
 
-def CB(lst1,lst2):
-    lst3=intersection(lst1,lst2)
-    print("\n\nList of students who play both cricket and badminton is : ", lst3)
-    return len(lst3)
+def CB(L1,L2):
+    L3=intersection(L1,L2)
+    print("\n List of students who play both cricket and badminton is : ", L3)
+    return len(L3)
 
 # Function for finding List of students who play either cricket or badminton but not both
 
-def eCeB(lst1,lst2):
-    lst3=sym_diff(lst1,lst2)
-    print("\nList of students who play either cricket or badminton but not both is : ",lst3)
-    return len(lst3)
+def eCeB(L1,L2):
+    L3=sym_diff(L1,L2)
+    print("\n List of students who play either cricket or badminton but not both is : ",L3)
+    return len(L3)
 
 # Function for finding Number of students who play neither cricket nor badminton
 
-def nCnB(lst1,lst2,lst3):
-    lst4=diff(lst1,union(lst2,lst3))
-    print("\n\nList of students who play neither cricket nor badminton is : ",lst4)
-    return len(lst4)
+def nCnB(L1,L2,L3):
+    L4=diff(L1,union(L2,L3))
+    print("\n List of students who play neither cricket nor badminton is : ",L4)
+    return len(L4)
 
 # Function for finding Number of students who play cricket and football but not badminton
 
-def CBnF(lst1,lst2,lst3):
-    lst4=diff(intersection(lst1,lst2),lst3)
-    print("\n\nList of students who play cricket and football but not badminton is : ",lst4)
-    return len(lst4)
+def CBnF(L1,L2,L3):
+    L4=diff(intersection(L1,L2),L3)
+    print("\n List of students who play cricket and football but not badminton is : ",L4)
+    return len(L4)
 
 # Main function
 
 # Creating an empty list for SE COMP
 SEComp = []
-n = int(input("\nEnter number of students in SE COMP: "))
+n = int(input("\n Enter number of students in SE COMP: "))
 print("Enter the names of",n,"students (Please press ENTER after entering each students name) :")
 for i in range(0, n):
-    ele = input()
-    SEComp.append(ele)  # adding the element
+    element = input()
+    SEComp.append(element)  # adding the element
 print("Original list of students in SEComp : " + str(SEComp))
 
 # Creating an empty list for Cricket
 Cricket = []
-n = int(input("\n\nEnter number of students who play cricket : "))
+n = int(input("\n Enter number of students who play cricket : "))
 print("Enter the names of",n,"students who play cricket (Please press ENTER after entering each students name) :")
 for i in range(0, n):
-    ele = input()
-    Cricket.append(ele)  # adding the element
+    element = input()
+    Cricket.append(element)  # adding the element
 print("Original list of students playing cricket is :" +str(Cricket))
 Cricket=removeDuplicate(Cricket)
 print("The list of students playing cricket after removing duplicates : " +str(Cricket))
 
 # Creating an empty list for Football
 Football = []
-n = int(input("\n\nEnter number of students who play football : "))
+n = int(input("\n Enter number of students who play football : "))
 print("Enter the name of",n,"students who play football (Please press ENTER after entering each students name) :")
 for i in range(0, n):
-    ele = input()
-    Football.append(ele)  # adding the element
+    element = input()
+    Football.append(element)  # adding the element
 print("Original list of students playing football :" +str(Football))
 Football=removeDuplicate(Football)
 print("The list of students playing football after removing duplicates : " +str(Football))
 
 # Creating an empty list for Badminton
 Badminton = []
-n = int(input("\n\nEnter number of students who play badminton : "))
+n = int(input("\n Enter number of students who play badminton : "))
 print("Enter the name of",n,"students who play badminton (Please press ENTER after entering each students name) :")
 for i in range(0, n):
-    ele = input()
-    Badminton.append(ele)  # adding the element
+    element = input()
+    Badminton.append(element)  # adding the element
 print("Original list of students playing badminton :" +str(Badminton))
 Badminton=removeDuplicate(Badminton)
 print("The list of students playing badminton after removing duplicates : " +str(Badminton))
@@ -127,12 +126,12 @@ while flag==1:
 
     if ch==1:
         print("Number of students who play both cricket and badminton : ", CB(Cricket,Badminton))
-        a = input("\n\nDo you want to continue (yes/no) :")
+        a = input("\n Do you want to continue (yes/no) :")
         if a == "yes":
             flag = 1
         else:
             flag = 0
-            print("Bye")
+            print("Thankyou!")
 
     elif ch==2:
         print("Number of students who play either cricket or badminton but not both : ", eCeB(Cricket, Badminton))
@@ -141,7 +140,7 @@ while flag==1:
             flag = 1
         else:
             flag = 0
-            print("Bye")
+            print("Thankyou!")
 
     elif ch==3:
         print("Number of students who play neither cricket nor badminton : ", nCnB(SEComp,Cricket,Badminton))
@@ -150,7 +149,7 @@ while flag==1:
             flag = 1
         else:
             flag = 0
-            print("Bye")
+            print("Thankyou!")
 
     elif ch==4:
         print("Number of students who play cricket and football but not badminton : ", CBnF(Cricket,Football,Badminton))
@@ -159,17 +158,17 @@ while flag==1:
             flag = 1
         else:
             flag = 0
-            print("Bye")
+            print("Thankyou!")
 
     elif ch==5:
         flag=0
-        print("Bye")
+        print("Thankyou!")
 
     else:
         print("!!Wrong Choice!! ")
-        a=input("\n\nDo you want to continue (yes/no) :")
+        a=input("\n Do you want to continue (yes/no) :")
         if a=="yes":
             flag=1
         else:
             flag=0
-            print("Bye")
+            print("Thankyou!")
